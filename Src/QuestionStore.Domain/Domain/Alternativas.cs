79b3d5371ea5de769d3d5bf0c53296a7300bc205
danieldrn <daniel.rn@live.com>
2019-09-21 19:07:10 -0300

    M006 - Inclusao de classe Alternativa

---
+++ Src/QuestionStore.Domain/Domain/Alternativas.cs
@@ -0,0 +1,12 @@
+﻿namespace QuestionStore.Domain.Domain
+{
+    public class Alternativas : Entity
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Letra { get; set; } 
+
+    }
+}
--- Src/QuestionStore.Domain/Domain/Question.cs
+++ Src/QuestionStore.Domain/Domain/Question.cs
@@ -1,9 +1,14 @@
-﻿namespace QuestionStore.Domain.Domain
+﻿using System.Collections.Generic;
+
+namespace QuestionStore.Domain.Domain
 {
     public class Question : Entity
     {
         public string Description { get; set; }
 
         public string Id { get; set; }
+
+        public List<Alternativas> Alternativas { get; set; }
+
     }
 }
