305d45e0025d8f7dc9a310cc8a3234483dbdb210
danieldrn <daniel.rn@live.com>
2019-12-10 22:55:42 -0300

    M028 - Inclusao de eventStore no projeto

---
+++ EventSource/IEventStoreService.cs
@@ -0,0 +1,12 @@
+﻿using EventStore.ClientAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventSource
+{
+    public interface IEventStoreService
+    {
+        IEventStoreConnection GetConection();
+    }
+}
