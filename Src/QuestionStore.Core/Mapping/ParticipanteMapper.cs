4e70b18a4ca660aae6f27d210bad61863e35c2a3
danieldrn <daniel.rn@live.com>
2020-02-05 19:22:30 -0300

    M050 - Ajustes no mapeamento participante

--- Src/QuestionStore.Core/Mapping/ParticipanteMapper.cs
+++ Src/QuestionStore.Core/Mapping/ParticipanteMapper.cs
@@ -4,6 +4,7 @@ using QuestionStore.Core.Service;
 using QuestionStore.Domain.Domain;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 
 namespace QuestionStore.Core.Mapping
 {
@@ -23,11 +24,14 @@ namespace QuestionStore.Core.Mapping
             using (var con = Connection.Factory.Crie(_configuration))
             using (var cmd = con.ObtenhaComando())
             {
-                var identificador = Guid.NewGuid().ObtenhaGuid();
+                var identificador = Guid.NewGuid().ToString();
 
-                cmd.CommandText = $@"INSERT INTO PARTICIPANTE (PARTCODIGO, PARTNAME) VALUES (@PARTCODIGO, @PARTNAME)";
-                cmd.AddParametersToCommand(new[] { "@PARTCODIGO", "@PARTNAME" });
-                cmd.AddValuesToParameters(new List<object> { identificador, comando.Nome });
+                cmd.CommandText =
+$@"INSERT INTO PARTICIPANTE (PARTCODIGO, PARTNAME, PARTIDADE, PARTCPF) 
+VALUES (@PARTCODIGO, @PARTNAME, @PARTIDADE, @PARTCPF)";
+
+                cmd.AddParametersToCommand(new[] { "@PARTCODIGO", "@PARTNAME", "@PARTIDADE", "@PARTCPF" });
+                cmd.AddValuesToParameters(new List<object> { identificador, comando.Nome, comando.Idade, comando.Cpf });
                 cmd.ExecuteNonQuery();
 
                 cmd.Commit();
@@ -41,19 +45,29 @@ namespace QuestionStore.Core.Mapping
             using (var con = Connection.Factory.Crie(_configuration))
             using (var cmd = con.ObtenhaComando())
             {
-                cmd.CommandText = "SELECT PARTNAME FROM PARTICIPANTE";
+                cmd.CommandText = "SELECT PARTNAME, PARTIDADE, PARTCPF, PARTCODIGO FROM PARTICIPANTE";
 
                 using (var dr = cmd.ExecuteReader())
                 {
                     while (dr.Read())
                     {
-                        participantes.Add(new Participante { Nome = dr.GetString(0) });
+                        participantes.Add(Mapeie(dr));
                     }
                 }
-
             }
 
             return participantes;
+        }
+
+        private static Participante Mapeie(DbDataReader dr)
+        {
+            return new Participante
+            {
+                Nome = dr.GetString(0),
+                Idade = dr.GetInt16(1),
+                CpfCnpj = dr.GetString(2),
+                Id = dr.GetString(3),
+            };
         }
     }
 
