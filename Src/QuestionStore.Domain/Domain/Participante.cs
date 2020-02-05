ef29cdf8c0f6331cb90d3c69f00b169175db02c9
danieldrn <daniel.rn@live.com>
2020-02-05 19:23:10 -0300

    M051 - Inclui novas propriedades na classe participante

--- Src/QuestionStore.Domain/Domain/Participante.cs
+++ Src/QuestionStore.Domain/Domain/Participante.cs
@@ -6,6 +6,38 @@ namespace QuestionStore.Domain.Domain
 {
     public class Participante : Entity
     {
+        public string Id
+        {
+            get { return base.Guid.ToString(); }
+            set { base.Guid = Guid.Parse(value); }
+        }
+
         public String Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public string CpfCnpj { get; set; }
+
+        #region Overrides
+        public override bool Equals(object obj)
+        {
+            var participante = obj as Participante;
+
+            return participante != null
+                && Id == participante.Id
+                && CpfCnpj == participante.CpfCnpj;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, CpfCnpj);
+        }
+
+        public override string ToString()
+        {
+            return $"Nome :{this.Nome} CPF :{this.CpfCnpj}";
+        }
+
+        #endregion
     }
 }
