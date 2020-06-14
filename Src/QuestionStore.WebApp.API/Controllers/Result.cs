72e161cbcd1579faa48656bbd2cf4fc8f45b82ff
danieldrn <daniel.rn@live.com>
2020-06-14 19:37:59 -0300

    M067 - Inclui classe Result

---
+++ Src/QuestionStore.WebApp.API/Controllers/Result.cs
@@ -0,0 +1,28 @@
+﻿using QuestionStore.Core.Commands;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuestionStore.WebApp.API.Controllers
+{
+    public class Result
+    {
+        private readonly Command _command;
+
+        public Result(Command command)
+        {
+            _command = command;
+        }
+
+        public Result(object data)
+        {
+            Data = data;
+        }
+
+        public List<string> Errors => _command?.Errors.Select(c => c.ErrorMessage).ToList() ?? new List<string>();
+
+        public bool Success => !Errors.Any();
+
+        public object Data { get; }
+    }
+
+}
